7e4577414a675423a0b205c09c1350021cbc7fab
ccg1120 <ccg.1120@daum.net>
2018-08-09 17:24:47 +0900

    commit Test

--- app/src/main/java/com/choe/androidsample/MainActivity.java
+++ app/src/main/java/com/choe/androidsample/MainActivity.java
@@ -42,14 +42,7 @@ public class MainActivity extends AppCompatActivity  {
 
         //Toast.makeText(this, "USB 연결", LENGTH_SHORT).show();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         Log.i(TAG,"OnCreate");
 
         TextView text = (TextView) findViewById(R.id.textView1);
